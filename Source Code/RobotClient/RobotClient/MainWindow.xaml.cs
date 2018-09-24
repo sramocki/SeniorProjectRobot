8073737972dc08a049fa04fc17c9d310df4cd7b0
Sean Ramocki <sramocki@oakland.edu>
2018-09-24 16:18:36 -0400

    Added example network packet and example references

--- Source Code/RobotClient/RobotClient/MainWindow.xaml.cs
+++ Source Code/RobotClient/RobotClient/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -17,6 +19,7 @@ namespace RobotClient
         public string FollowerMAC;
 
         private bool _dragging;
+        private double sliderSpeed = 0;
 
 
         public MainWindow()
@@ -46,27 +49,26 @@ namespace RobotClient
             //TODO
         }
 
-        private void MoveEvent_Click(object sender, RoutedEventArgs e)
+        private void ButtonPress_Event(object sender, RoutedEventArgs e)
         {
             var button = (Button)sender;
-            //Console.WriteLine(button.Content);
             switch (button.Content)
             {
                 case "Forward":
-                    LogField.AppendText("Moving forward\n");
+                    LogField.AppendText("Moving forward at " + sliderSpeed + "\n");
                     //send data here
                     break;
 
                 case "Backwards":
-                    LogField.AppendText("Moving backwards\n");
+                    LogField.AppendText("Moving backwards at " + sliderSpeed + "\n");
                     break;
 
                 case "Left":
-                    LogField.AppendText("Moving left\n");
+                    LogField.AppendText("Moving left at " + sliderSpeed + "\n");
                     break;
 
                 case "Right":
-                    LogField.AppendText("Moving right\n");
+                    LogField.AppendText("Moving right at " + sliderSpeed + "\n");
                     break;
 
                 default:
@@ -75,7 +77,22 @@ namespace RobotClient
                     break;
 
             }
+
+            //make leaderIP an array since there can be more than one leader at a time
+            //this transmission needs to be on its own thread so the packets can be sent simultaneously
+            //https://gist.github.com/leandrosilva/656054
+            //https://stackoverflow.com/questions/21549963/tcp-server-c-sharp-on-windows-tcp-client-python-raspberry-pi
+            //https://stackoverflow.com/questions/2637697/sending-udp-packet-in-c-sharp
+            //https://codereview.stackexchange.com/questions/147606/simple-multi-client-echo-server
+
+            SendUdp(11000, LeaderIp, 11000, Encoding.ASCII.GetBytes("Hello!"));
             LogField.ScrollToEnd();
+        }
+
+        static void SendUdp(int srcPort, string dstIp, int dstPort, byte[] data)
+        {
+            using (UdpClient c = new UdpClient(srcPort))
+                c.Send(data, data.Length, dstIp, dstPort);
         }
 
         private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
@@ -84,8 +101,8 @@ namespace RobotClient
             if (!_dragging)
             {
                 var slider = sender as Slider;
-                double value = Math.Ceiling(slider.Value);
-                LogField.AppendText("Setting speed at " + value + "\n");
+                sliderSpeed = Math.Ceiling(slider.Value);
+                LogField.AppendText("Setting speed at " + sliderSpeed + "\n");
                 LogField.ScrollToEnd();
             }
         }
@@ -100,8 +117,8 @@ namespace RobotClient
             if (!_dragging)
             {
                 var slider = sender as Slider;
-                double value = Math.Ceiling(slider.Value);
-                LogField.AppendText("Setting speed at " + value + "\n");
+                sliderSpeed = Math.Ceiling(slider.Value);
+                LogField.AppendText("Setting speed at " + sliderSpeed + "\n");
                 LogField.ScrollToEnd();
             }
         }
