3b7b31a25f4a455a0e20afce00a7578ccafebf8b
Sean Ramocki <sramocki@oakland.edu>
2018-09-25 13:39:24 -0400

    Added keyboard input and formalized data processing for networking

--- Source Code/RobotClient/RobotClient/MainWindow.xaml.cs
+++ Source Code/RobotClient/RobotClient/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -11,15 +13,22 @@ namespace RobotClient
 
     public partial class MainWindow : Window
     {
+
+        public List<String[]> LeaderIpList = new List<string[]>();
         public string LeaderIp { set; get; }
         public string FollowerIP { set; get; }
+        private bool _keyHold = false;
 
-        //unused for now
-        public string LeaderMAC;
-        public string FollowerMAC;
+        //TODO allow multi direction keyboard presses
+        private bool keyone = false;
+        private bool keytwo = false;
+
+        //TODO
+        public string LeaderMac;
+        public string FollowerMac;
 
         private bool _dragging;
-        private double sliderSpeed = 0;
+        private double _sliderSpeed = 0;
 
 
         public MainWindow()
@@ -30,7 +39,8 @@ namespace RobotClient
 
             var newKeybind = new RoutedCommand();
             newKeybind.InputGestures.Add(new KeyGesture(Key.R, ModifierKeys.Control));
-            CommandBindings.Add(new CommandBinding(newKeybind, Register_Click));
+            CommandBindings.Add(new CommandBinding(newKeybind, Register_Click));;
+
         }
 
         private void Register_Click(object sender, RoutedEventArgs e)
@@ -49,26 +59,73 @@ namespace RobotClient
             //TODO
         }
 
+        private void Key_up(object sender, KeyEventArgs e)
+        {
+            _keyHold = false;
+            LogField.AppendText("Stopped moving!\n");
+            LogField.ScrollToEnd();
+        }
+
+        private void Key_down(object sender, KeyEventArgs e)
+        {
+            //TODO XInput and allow multi direction key presses
+            if (e.Key == Key.W || e.Key == Key.Up)
+            {
+                _keyHold = true;
+                LogField.AppendText("Moving forward at " + _sliderSpeed + "\n");
+                //Send packet L_0.0_1.0
+                LogField.ScrollToEnd();
+            }
+            else if (e.Key == Key.S || e.Key == Key.Down)
+            {
+                _keyHold = true;
+                LogField.AppendText("Moving backwards at " + _sliderSpeed + "\n");
+                //Send packet L_0.0_-1.0
+                LogField.ScrollToEnd();
+            }
+            else if (e.Key == Key.A || e.Key == Key.Left)
+            {
+                _keyHold = true;
+                LogField.AppendText("Moving left at " + _sliderSpeed + "\n");
+                //Send packet L_-1.0_0.0
+                LogField.ScrollToEnd();
+            }
+            else if (e.Key == Key.D || e.Key == Key.Right)
+            {
+                _keyHold = true;
+                LogField.AppendText("Moving right at " + _sliderSpeed + "\n");
+                //Send packet L_1.0_0.0
+                LogField.ScrollToEnd();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
         private void ButtonPress_Event(object sender, RoutedEventArgs e)
         {
-            var button = (Button)sender;
+            var button = (RepeatButton)sender;
             switch (button.Content)
             {
                 case "Forward":
-                    LogField.AppendText("Moving forward at " + sliderSpeed + "\n");
-                    //send data here
+                    LogField.AppendText("Moving forward at " + _sliderSpeed + "\n");
+                    //Send packet L_0.0_1.0
                     break;
 
                 case "Backwards":
-                    LogField.AppendText("Moving backwards at " + sliderSpeed + "\n");
+                    LogField.AppendText("Moving backwards at " + _sliderSpeed + "\n");
+                    //Send packet L_0.0_-1.0
                     break;
 
                 case "Left":
-                    LogField.AppendText("Moving left at " + sliderSpeed + "\n");
+                    LogField.AppendText("Moving left at " + _sliderSpeed + "\n");
+                    //Send packet L_-1.0_0.0
                     break;
 
                 case "Right":
-                    LogField.AppendText("Moving right at " + sliderSpeed + "\n");
+                    LogField.AppendText("Moving right at " + _sliderSpeed + "\n");
+                    //Send packet L_1.0_0.0
                     break;
 
                 default:
@@ -77,32 +134,22 @@ namespace RobotClient
                     break;
 
             }
-
-            //make leaderIP an array since there can be more than one leader at a time
-            //this transmission needs to be on its own thread so the packets can be sent simultaneously
-            //https://gist.github.com/leandrosilva/656054
-            //https://stackoverflow.com/questions/21549963/tcp-server-c-sharp-on-windows-tcp-client-python-raspberry-pi
-            //https://stackoverflow.com/questions/2637697/sending-udp-packet-in-c-sharp
-            //https://codereview.stackexchange.com/questions/147606/simple-multi-client-echo-server
-
-            SendUdp(11000, LeaderIp, 11000, Encoding.ASCII.GetBytes("Hello!"));
             LogField.ScrollToEnd();
         }
 
-        static void SendUdp(int srcPort, string dstIp, int dstPort, byte[] data)
+/*        static void SendUdp(int srcPort, string dstIp, int dstPort, byte[] data)
         {
             using (UdpClient c = new UdpClient(srcPort))
                 c.Send(data, data.Length, dstIp, dstPort);
-        }
+        }*/
 
         private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             _dragging = false;
             if (!_dragging)
             {
-                var slider = sender as Slider;
-                sliderSpeed = Math.Ceiling(slider.Value);
-                LogField.AppendText("Setting speed at " + sliderSpeed + "\n");
+                if (sender is Slider slider) _sliderSpeed = Math.Ceiling(slider.Value);
+                LogField.AppendText("Setting speed at " + _sliderSpeed + "\n");
                 LogField.ScrollToEnd();
             }
         }
@@ -116,9 +163,8 @@ namespace RobotClient
         {
             if (!_dragging)
             {
-                var slider = sender as Slider;
-                sliderSpeed = Math.Ceiling(slider.Value);
-                LogField.AppendText("Setting speed at " + sliderSpeed + "\n");
+                if (sender is Slider slider) _sliderSpeed = Math.Ceiling(slider.Value);
+                LogField.AppendText("Setting speed at " + _sliderSpeed + "\n");
                 LogField.ScrollToEnd();
             }
         }
