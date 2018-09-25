e1e6fefbce573cec63258e7a96ae864514448076
eramocki <32600668+eramocki@users.noreply.github.com>
2018-09-24 20:21:44 -0400

    Reworked Registration behind the scenes

--- Source Code/RobotClient/RobotClient/Registration.xaml.cs
+++ Source Code/RobotClient/RobotClient/Registration.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,79 +21,129 @@ namespace RobotClient
     /// </summary>
     public partial class Registration : Window
     {
+        List<String[]> ipStringList = new List<string[]>();
         MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
         public Registration()
         {
             InitializeComponent();
             ipList.ItemsSource = null;
+
+            ipStringList = mainWindow.LeaderIpList;
+            ipList.ItemsSource = ipStringList.Select(array => array.FirstOrDefault());
         }
 
-        private void ExampleButton(object sender, RoutedEventArgs e)
-        {
-            mainWindow.IPTextBlock.Text = "POOP";
-            mainWindow.LeaderIp = "POOP";
 
-            mainWindow.FollowerIP = "";
-        }
 
         private void ButtonScan(object sender, RoutedEventArgs e)
         {
-            List<String> ipStringList = new List<string>();
+
+            ipStringList.Clear();
+            var defaultGateway = "";
+            foreach (NetworkInterface curInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (curInterface.OperationalStatus != OperationalStatus.Up) continue;
+                foreach (GatewayIPAddressInformation gatewayOutput in curInterface.GetIPProperties()
+                    .GatewayAddresses)
+                {
+                    defaultGateway = gatewayOutput.Address.ToString();
+                }
+
+            }
+
+            defaultGateway = defaultGateway.Substring(0, (defaultGateway.Length - 1));
+            Console.WriteLine(defaultGateway);
+            
             for (int i = 1; i < 20; i++)
             {
-                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
-                System.Net.NetworkInformation.PingReply rep = p.Send("192.168.1."+i);
-                if (rep.Status == System.Net.NetworkInformation.IPStatus.Success)
+                Ping p = new System.Net.NetworkInformation.Ping();
+                PingReply rep = p.Send(defaultGateway + i);
+                if (rep.Status == IPStatus.Success)
                 {
-                    Console.WriteLine("IP Found at 192.168.1." + i);
-                    ipStringList.Add("192.168.1." + i);
+                    Console.WriteLine("IP Found at " + defaultGateway + i);
+                    String deviceName = "";
+                    try
+                    {
+                       
+                        deviceName = Dns.GetHostEntry(defaultGateway + i).HostName.ToString();
+                        
+                    }
+                    catch
+                    {
+                        deviceName = "unknown";
+
+                    }
+
+                    Console.WriteLine("Device Name is: " + deviceName);
+
+                    ipStringList.Add(new String[] { defaultGateway + i, deviceName, "Default" });
                 }
             }
-            ipList.ItemsSource = ipStringList;
+
+
+            ipList.ItemsSource = ipStringList.Select(array => array.FirstOrDefault());
+            mainWindow.LeaderIpList = ipStringList;
+
         }
 
         private void ipList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            try
-            {
-                String ipAddress = ipList.SelectedItem.ToString();
-                String machineName = Dns.GetHostEntry(ipAddress).HostName.ToString();
-                macAddressBox.Text = machineName;
-            }
-            catch
-            {
-                macAddressBox.Text = "unknown";
 
-            }
+
+                String ipString = ipList.SelectedItem.ToString();
+
+            int index = ipStringList.FindIndex(array => array[0] == ipString);
+            //Console.WriteLine(index); Line used for Testing
+
+            macAddressBox.Text= ipStringList[index][1].ToString();
+            roleBox.Text = ipStringList[index][2].ToString();
+
 
         }
         private void LeaderButton(object sender, RoutedEventArgs e)
         {
+            String ipString = ipList.SelectedItem.ToString();
 
-            
+            int index = ipStringList.FindIndex(array => array[0] == ipString);
+
+            ipStringList[index][2] = "Leader";
+
+            roleBox.Text = ipStringList[index][2].ToString();
+
             mainWindow.IPTextBlock.Text = ipList.SelectedItem.ToString();
-            mainWindow.LeaderIp = ipList.SelectedItem.ToString();
+            mainWindow.LeaderIpList = ipStringList;
 
-            
+
         }
 
         private void FollowerRoleButton(object sender, RoutedEventArgs e)
         {
 
+            String ipString = ipList.SelectedItem.ToString();
 
-            mainWindow.IPTextBlock.Text = ipList.SelectedItem.ToString();
-            mainWindow.FollowerIP = ipList.SelectedItem.ToString();
+            int index = ipStringList.FindIndex(array => array[0] == ipString);
 
+            ipStringList[index][2] = "Follower";
+
+            roleBox.Text = ipStringList[index][2].ToString();
+            // mainWindow.IPTextBlock.Text = ipList.SelectedItem.ToString();
+            // mainWindow.FollowerIP = ipList.SelectedItem.ToString();
+            mainWindow.LeaderIpList = ipStringList;
 
         }
 
         private void DefaultRoleButton(object sender, RoutedEventArgs e)
         {
 
+            String ipString = ipList.SelectedItem.ToString();
 
-            mainWindow.IPTextBlock.Text = ipList.SelectedItem.ToString();
-            mainWindow.FollowerIP = ipList.SelectedItem.ToString();
+            int index = ipStringList.FindIndex(array => array[0] == ipString);
 
+            ipStringList[index][2] = "Default";
+
+            roleBox.Text = ipStringList[index][2].ToString();
+            // mainWindow.IPTextBlock.Text = ipList.SelectedItem.ToString();
+            // mainWindow.FollowerIP = ipList.SelectedItem.ToString();
+            mainWindow.LeaderIpList = ipStringList;
 
         }
     }
