01d3186ff6d554c73b4683b40136f9671dac2b06
Christian Nickolaou <nickolaou@oakland.edu>
2018-09-30 21:34:03 -0400

    Added algorithm for camshift to be performed after region is selected

--- Source Code/CollisionDetection/CamShift.py
+++ Source Code/CollisionDetection/CamShift.py
@@ -24,5 +24,95 @@ def main():
     ap.add_argument("-v", "--video", help = "path to the (optional) video file")
     args = vars(ap.parse_args())
 
+    # get references to current frame, roiPts, and if in selection mode
+    global frame, roiPts, inputMode
+
+    # if there is no path supplied, then grab reference to camera
+    if not args.get("video", False):
+        camera = cv2.VideoCapture(0)
+
+    # otherwise, load the video
+    else:
+        camera = cv2.VideoCapture(args["video"])
+
+    # set callback
+    cv2.namedWindow("frame")
+    cv2.setMouseCallback("frame", selectROI)
+
+    # initialize the termination criteria for cam shift, indicating
+    # a maximum of ten iterations or movement by at least one pixel
+    # along with the bounding box of the ROI
+    termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+    roiBox = None
+
+    # loop over frames
+    while True:
+        # grab the current frame
+        (grabbed, frame) = camera.read()
+
+        # check to see if we have reached the end of the video
+        if not grabbed:
+            break
+
+        # resize the video (this is harcoded)
+        frame = cv2.resize(frame, (400, 200))
+
+        # check to see if the ROI has been computed
+        if roiBox is not None:
+            # convert the current frame to the HSV color space
+            # and perform mean shift
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            backProj = cv2.calcBackProject([hsv], [0], roiHist, [0, 180], 1)
+
+            # apply cam shift to the back projection, convert the
+            # points to a bounding box, and then draw them
+            (r, roiBox) = cv2.CamShift(backProj, roiBox, termination)
+            pts = np.int0(cv2.cv.BoxPoints(r))
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+
+        # show the frame and record if the user presses a key
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # handle if the 'i' key is pressed, then go 
+        # into ROI selection mode
+        if key == ord("i") and len(roiPts) < 4:
+            # indicate that we are in input mode and clone the frame
+            inputMode = True
+            orig = frame.copy()
+
+            # keep looping until all ROI points have
+            # been selected; press any key to exit ROI selection
+            # mode once points are selected
+            while len(roiPts) < 4:
+                cv2.imshow("frame", frame)
+                cv2.waitKey(0)
+
+            # determine the top-left and bottom-right points
+            roiPts = np.array(roiPts)
+            s = roiPts.sum(axis = 1)
+            tl = roiPts[np.argmin(s)]
+            br = roiPts[np.argmax(s)]
+
+            # grab the ROI for the bounding box and convert it
+            # to the HSV color space
+            roi = orig[tl[1]:br[1], tl[0]:br[0]]
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            #roi = cv2.cvtColor(roi cv2.COLOR_BGR2LAB)
+
+            # compute a HSV histogram for the ROI and store the
+            # bounding box
+            roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
+            roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
+            roiBox = (tl[0], tl[1], br[0], br[1])
+
+        # if the 'q' key is pressed, stop the loop
+        elif key == ord("q"):
+            break
+
+    # cleanup the camera and close any open windows
+    camera.release()
+    cv2.destroyAllWindows
+
 if __name__ == "__main__":
     main()
