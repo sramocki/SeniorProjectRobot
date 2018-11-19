5550c52baae03b82e61b903e9d8bb0892ffd866f
Christian Nickolaou <nickolaou@oakland.edu>
2018-11-19 09:16:12 -0500

    added global variables for corners to print out

--- Source/RobotServer/singleimgcap.py
+++ Source/RobotServer/singleimgcap.py
@@ -5,6 +5,11 @@ import argparse
 arDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
 parameters = cv2.aruco.DetectorParameters_create()
 
+tLeft = None
+tRight = None
+bLeft = None
+bRight = None
+
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--file", help="name of the file saved")
@@ -30,6 +35,11 @@ def main():
 
         cv2.imshow('img', frame)
         if cv2.waitKey(1) & 0xFF == ord('y'):
+            
+            print("Top Left: %f, %f" % (tLeft[0], tLeft[1]))
+            print("Top Right: %f, %f" % (tRight[0], tRight[1]))
+            print("Bottom Right: %f, %f" % (bRight[0], bRight[1]))
+            print("Bottom Left: %f, %f" % (bLeft[0], bLeft[1]))
             cv2.imwrite(filePath, frame)
             cv2.destroyAllWindows()
             break
