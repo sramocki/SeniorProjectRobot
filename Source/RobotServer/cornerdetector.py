8af29080d6f4ebf80e3d490d62d81dcdf2808520
Christian Nickolaou <nickolaou@oakland.edu>
2018-11-19 08:51:44 -0500

    changed print statements and added error check

--- Source/RobotServer/cornerdetector.py
+++ Source/RobotServer/cornerdetector.py
@@ -11,18 +11,19 @@ def main():
 
     fileName = args["file"]
 
-    img = cv2.imread(fileName)
+    img = cv2.imread(fileName, 0)
     corners = getCorners(img)
     if corners is not None:
         tLeft = corners[0][0][0]
         tRight = corners[0][0][1]
         bRight = corners[0][0][2]
         bLeft = corners[0][0][3]
-        print "Top Left: {x}, {y}".format(x=tLeft[0], y=tLeft[1])
-        print "Top Right: {x}, {y}".format(x=tRight[0], y=tRight[1])
-        print "Bottom Right: {x}, {y}".format(x=bRight[0], y=bRight[1])
-        print "Bottom Left: {x}, {y}".format(x=bLeft[0], y=bLeft[1])
-
+        print "Top Left: {}, {}".format(tLeft[0], tLeft[1])
+        print "Top Right: {}, {}".format(tRight[0], tRight[1])
+        print "Bottom Right: {}, {}".format(bRight[0], bRight[1])
+        print "Bottom Left: {}, {}".format(bLeft[0], bLeft[1])
+    else:
+        print "No corners detected!"
 
 def getCorners(img):
     grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
